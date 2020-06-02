c9190f3012c95ef67ed1277f5c6c546707b4f700
SarnaKhmel <olsarnat@gmail.com>
2020-06-02 04:41:20 +0300

    pie chart fin

--- app/src/main/java/com/oleksa_sarnatskyi/parsesitedata/PieChartActivity.java
+++ app/src/main/java/com/oleksa_sarnatskyi/parsesitedata/PieChartActivity.java
@@ -2,7 +2,16 @@ package com.oleksa_sarnatskyi.parsesitedata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
 
 public class PieChartActivity extends AppCompatActivity {
 
@@ -10,5 +19,29 @@ public class PieChartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart);
+
+        PieChart pieChart = findViewById(R.id.pieChart);
+
+        ArrayList<PieEntry> visitorsAge = new ArrayList<>();
+        visitorsAge.add(new PieEntry(1462, "0 - 10"));
+        visitorsAge.add(new PieEntry(1828, "10 - 20"));
+        visitorsAge.add(new PieEntry(1096, "20 - 30"));
+        visitorsAge.add(new PieEntry(731, "30 - 50"));
+        visitorsAge.add(new PieEntry(2193, "50 - 100"));
+
+        PieDataSet pieDataSet = new PieDataSet(visitorsAge, "Вік відвідувачів");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Вік відвідувачів");
+        pieChart.animate();
+
+
+
     }
 }
--- app/src/main/java/com/oleksa_sarnatskyi/parsesitedata/RadianChartActivity.java
+++ app/src/main/java/com/oleksa_sarnatskyi/parsesitedata/RadianChartActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.RadarChart;
+
 public class RadianChartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radian_chart);
+
+        RadarChart radarChart = findViewById(R.id.radarChart);
+
+
     }
+
 }
